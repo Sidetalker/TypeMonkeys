b8e4a57c6bff3e2fb84cbdcd650288eb644cf498
Kevin Sullivan <admin@sideapps.com>
2015-04-13 19:16:26 -0400

    Dynamic Progress

--- public/core.js
+++ public/core.js
@@ -1,56 +1,7 @@
-var letterDisplayIndex = 1
+var letterDisplayState = 1
 var maxLetterCount = 0	
 
-		$(window).ready(function(e){
-      $.each($('div.progress-bar'),function(){
-        $(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
-      });
-});
-
 app = angular.module('typeMonkeys', ['timer', 'ui.bootstrap'])
-
-app.controller('ProgressDemoCtrl', function ($scope) {
-  $scope.max = 200;
-
-  $scope.update = function() {
-  	console.log('asdasf')
-  }
-
-  $scope.random = function() {
-    var value = Math.floor((Math.random() * 100) + 1);
-    var type;
-
-    if (value < 25) {
-      type = 'success';
-    } else if (value < 50) {
-      type = 'info';
-    } else if (value < 75) {
-      type = 'warning';
-    } else {
-      type = 'danger';
-    }
-
-    $scope.showWarning = (type === 'danger' || type === 'warning');
-
-    $scope.dynamic = value;
-    $scope.type = type;
-  };
-  $scope.random();
-
-  $scope.randomStacked = function() {
-    $scope.stacked = [];
-    var types = ['success', 'info', 'warning', 'danger'];
-
-    for (var i = 0, n = Math.floor((Math.random() * 4) + 1); i < n; i++) {
-        var index = Math.floor((Math.random() * 4));
-        $scope.stacked.push({
-          value: Math.floor((Math.random() * 30) + 1),
-          type: types[index]
-        });
-    }
-  };
-  $scope.randomStacked();
-});
 
 app.controller('CollapseController', ['$scope', function (sc) {
 	sc.isCollapsed = false
@@ -68,39 +19,120 @@ var monkey = app.controller('MonkeyController', ['$scope', function(sc) {
 
 	sc.letterCount = 0
 	sc.letterQuantities = []
+	sc.letterPercents = []
+
+	sc.timerRunning = true
 
 	for (var i = 0; i < alphabet.length; i++) {
 		sc.letterQuantities.push(0)
 	}
 
-	sc.timerRunning = true
+	console.log('Generating alphabet progress bars')
+
+	var code = ""
+	var max = 0
+
+	for (var i = 0; i < alphabet.length; i++) {
+		if (sc.letterQuantities[i] > max) {
+			max = sc.letterQuantities[i]
+		}
+	}
 
 	var getProgressBarData = function(index, max) {
 		var cur = sc.letterQuantities[index]
-		var type = "progress-bar progress-bar-info active"
-		var text = ""
+		var myClass = "progress-bar progress-bar-info ease-in-out"
+		var myText = '<b>' + alphabet[index].toUpperCase() + '</b>'
+		var myValue = sc.letterQuantities[index] / max * 100 + "%"
 
 		if (cur >= max * 0.7) {
-			type = "progress-bar progress-bar-success active six-sec-ease-in-out"
+			myClass = "progress-bar progress-bar-success ease-in"
 		}
 		else if (cur >= max * 0.4) {
-			type = "progress-bar progress-bar-warning active"
+			myClass = "progress-bar progress-bar-warning ease-in"
 		}
 		else {
-			type = "progress-bar progress-bar-danger active"
+			myClass = "progress-bar progress-bar-danger ease-in"
 		}
 
-		if (cur > 0) {
-			text = alphabet[index].toUpperCase()
+		switch(letterDisplayState) {
+			case 0:
+				myText = ""
+				break;
+			case 1:
+				myText = '<b>' + alphabet[index].toUpperCase() + '</b>'
+				break;
+			case 2:
+				myText = '<b>' + cur + '</b>'
+				break;
+			case 3:
+				myText = '<b>' + myValue + '</b>'
+			default:
+				console.log("Invalid letterDisplayState")
 		}
 
-		return [type, text, sc.letterQuantities[index]]
+		if (sc.letterQuantities[index] == 0) {
+			myText = ""
+		}
+
+		var myID = "progressBar" + index
+
+		return [myID, myClass, myText, myValue]
 	}
+
+	for (var i = 0; i < alphabet.length; i++) {
+		if (i + 2 < alphabet.length) {
+			code += '<div class="row">\n'
+        	code += '	<div class="col-md-4">\n'
+			code += '		<div class="progress">\n'
+			code += '			<div id="progressBar' + i + '" class="progress-bar progress-bar-info ease-in" aria-valuetransitiongoal="0">'
+        	code += '				<span ><b><div id="progressBarLabel' + i + '"></div></b></span>\n'
+        	code += '			</div>\n'
+      		code += '		</div>\n'
+      		code += '	</div>'
+      		i++
+        	code += '	<div class="col-md-4">\n'
+			code += '		<div class="progress">\n'
+			code += '			<div id="progressBar' + i + '" class="progress-bar progress-bar-info ease-in" aria-valuetransitiongoal="0">'
+        	code += '				<span ><b><div id="progressBarLabel' + i + '"></div></b></span>\n'
+        	code += '			</div>\n'
+      		code += '		</div>\n'
+      		code += '	</div>'
+      		i++
+        	code += '	<div class="col-md-4">\n'
+			code += '		<div class="progress">\n'
+			code += '			<div id="progressBar' + i + '" class="progress-bar progress-bar-info ease-in" aria-valuetransitiongoal="0">'
+        	code += '				<span ><b><div id="progressBarLabel' + i + '"></div></b></span>\n'
+        	code += '			</div>\n'
+      		code += '		</div>\n'
+      		code += '	</div>'
+      		code += '</div>'
+		}
+		else {
+			code += '<div class="row">\n'
+        	code += '	<div class="col-md-6">\n'
+			code += '		<div class="progress">\n'
+			code += '			<div id="progressBar' + i + '" class="progress-bar progress-bar-info ease-in" aria-valuetransitiongoal="0">'
+        	code += '				<span ><b><div id="progressBarLabel' + i + '"></div></b></span>\n'
+        	code += '			</div>\n'
+      		code += '		</div>\n'
+      		code += '	</div>'
+      		i++
+        	code += '	<div class="col-md-6">\n'
+			code += '		<div class="progress">\n'
+			code += '			<div id="progressBar' + i + '" class="progress-bar progress-bar-info ease-in" aria-valuetransitiongoal="0">'
+        	code += '				<span ><b><div id="progressBarLabel' + i + '"></div></b></span>\n'
+        	code += '			</div>\n'
+      		code += '		</div>\n'
+      		code += '	</div>'
+      		code += '</div>'
+		}
+	}
+
+	document.getElementById("progress").innerHTML = code
 
 	var updateProgressBars = function() {
 		console.log('Updating progress bars')
 
-		var code = ""
 		var max = 0
 
 		for (var i = 0; i < alphabet.length; i++) {
@@ -109,92 +141,14 @@ var monkey = app.controller('MonkeyController', ['$scope', function(sc) {
 			}
 		}
 
-		maxLetterCount = max
+		for (var i = 0; i < alphabet.length; i++) {
+			var data = getProgressBarData(i, max)
 
-		for (var i = 0; i < alphabet.length; i+=3) {
-			if (i + 2 < alphabet.length) {
-				var data1 = getProgressBarData(i, max)
-				var data2 = getProgressBarData(i + 1, max)
-				var data3 = getProgressBarData(i + 2, max)
-
-				code += '<div class="row">\n'
-	        	code += '	<div class="col-md-4">\n'
-				code += '		<div class="progress">\n'
-				code += '			<div class="' + data1[0] + '" role="progressbar" aria-valuenow="' + data1[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data1[2] / max * 100) + '%">\n'
-	        	code += '				<span ><b>' + data1[1] + '</b></span>\n'
-	        	code += '			</div>\n'
-	      		code += '		</div>\n'
-	      		code += '	</div>'
-	        	code += '	<div class="col-md-4">\n'
-				code += '		<div class="progress">\n'
-				code += '			<div class="' + data2[0] + '" role="progressbar" aria-valuenow="' + data2[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data2[2] / max * 100) + '%">\n'
-	        	code += '				<span ><b>' + data2[1] + '</b></span>\n'
-	        	code += '			</div>\n'
-	      		code += '		</div>\n'
-	      		code += '	</div>'
-	        	code += '	<div class="col-md-4">\n'
-				code += '		<div class="progress">\n'
-				code += '			<div class="' + data3[0] + '" role="progressbar" aria-valuenow="' + data3[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data3[2] / max * 100) + '%">\n'
-	        	code += '				<span ><b>' + data3[1] + '</b></span>\n'
-	        	code += '			</div>\n'
-	      		code += '		</div>\n'
-	      		code += '	</div>'
-	      		code += '</div>'
-			}
-			else {
-				var data1 = getProgressBarData(i, max)
-				var data2 = getProgressBarData(i + 1, max)
-
-				code += '<div class="row">\n'
-	        	code += '	<div class="col-md-6">\n'
-				code += '		<div class="progress">\n'
-				code += '			<div class="progress-bar six-sec-ease-in-out" aria-valuetransitiongoal="20">\n'
-				// code += '			<div class="' + data1[0] + '" aria-valuenow="' + data1[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data1[2] / max * 100) + '%">\n'
-	        	code += '				<span ><b>' + data1[1] + '</b></span>\n'
-	        	code += '			</div>\n'
-	      		code += '		</div>\n'
-	      		code += '	</div>'
-	        	code += '	<div class="col-md-6">\n'
-				code += '		<div class="progress">\n'
-				code += '			<div class="' + data2[0] + '" role="progressbar" aria-valuenow="' + data2[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data2[2] / max * 100) + '%">\n'
-	        	code += '				<span ><b>' + data2[1] + '</b></span>\n'
-	        	code += '			</div>\n'
-	      		code += '		</div>\n'
-	      		code += '	</div>'
-	      		code += '</div>'
-			}
-
-
-
-
-			// else {
-			// 	var data1 = getProgressBarData(i, max)
-			// 	var data2 = getProgressBarData(i + 1, max)
-
-			// 	code += '<div class="row">\n'
-	  //       	code += '	<div class="col-md-6">\n'
-			// 	code += '		<div class="progress">\n'
-			// 	code += '			<div class="' + data1[0] + '" aria-valuenow="' + data1[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data1[2] / max * 100) + '%">\n'
-	  //       	code += '				<span ><b>' + data1[1] + '</b></span>\n'
-	  //       	code += '			</div>\n'
-	  //     		code += '		</div>\n'
-	  //     		code += '	</div>'
-	  //       	code += '	<div class="col-md-6">\n'
-			// 	code += '		<div class="progress">\n'
-			// 	code += '			<div class="' + data2[0] + '" role="progressbar" aria-valuenow="' + data2[2] + '" aria-valuemin="0" aria-valuemax="' + max + '" style="width: ' + (data2[2] / max * 100) + '%">\n'
-	  //       	code += '				<span ><b>' + data2[1] + '</b></span>\n'
-	  //       	code += '			</div>\n'
-	  //     		code += '		</div>\n'
-	  //     		code += '	</div>'
-	  //     		code += '</div>'
-			// }
+			document.getElementById("progressBar" + i).className = data[1]
+			document.getElementById("progressBar" + i).style.width = data[3]
+			document.getElementById("progressBarLabel" + i).innerHTML = data[2]
 		}
-
-		document.getElementById("test").style.width = "90%"
-		console.log(document.getElementById("test"))
 	}
-
-	updateProgressBars()
 
 	sc.$on('update', function(event, index) {
 		updateProgressBars()
@@ -214,8 +168,6 @@ var monkey = app.controller('MonkeyController', ['$scope', function(sc) {
 
 	sc.manualLetter = function(event) {
 		if (event.charCode >= 97 && event.charCode <= 122) {
-			console.log(alphabet[event.charCode - 97])
-
 			sc.letterQuantities[event.charCode - 97]++
 			sc.letterCount++
 
@@ -241,7 +193,7 @@ app.controller('TextToggleController', ['$scope', function(sc) {
 	}
 
 	sc.update = function(index) {
-		letterDisplayIndex = index
+		letterDisplayState = index
 
 		sc.$emit('update', index);
 	}
