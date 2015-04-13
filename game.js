0b3c6761c2995871afb25b18628e001629645c7c
Kevin Sullivan <admin@sideapps.com>
2015-04-13 09:41:33 -0400

    Favicon Attempt

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // Initialize
 var express  = require('express');
 var app      = express();                           // create our app w/ express
+var favicon = require('serve-favicon');				// serves our favicon
 var morgan = require('morgan');                     // log requests to the console (express4)
 var mongoose = require('mongoose');                 // mongoose for mongodb
 var bodyParser = require('body-parser');            // pull information from HTML POST (express4)
@@ -8,7 +9,8 @@ var methodOverride = require('method-override');    // simulate DELETE and PUT (
 
 // Configure app
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.static(__dirname + '/public'));     
 app.use(morgan('dev'));                             // log every request to the console
 
 app.get('*', function(req, res) {
